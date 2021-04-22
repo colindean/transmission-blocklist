6911733b863e5de52fab1ba8940a6cd1fd3a3878
Colin Dean <git@cad.cx>
2021-04-21 23:36:05 -0400

    Optionally use aria2c if present unless NO_CACHE is passed

--- blocklist-build.sh
+++ blocklist-build.sh
@@ -1,8 +1,35 @@
 #!/bin/sh
-curl -s https://www.iblocklist.com/lists.json | \
-    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' | \
-    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' | \
-    xargs wget -O - | \
-    gunzip | \
-    egrep -v '^#' | \
-    gzip > blocklist.gz
+: "${NO_CACHE:-}"
+# Use aria2c if available for ~300% speedup, mostly from parallel downloading
+if [ -z "${NO_CACHE}" ] && [ -n "$(command -v aria2c)" ]; then
+  # however, aria2c can't output to stdout, so we have to write to a cache directory
+  CACHE=$(mktemp -d cache-XXXXXX)
+  mkdir -p "${CACHE}"
+
+  curl -s https://www.iblocklist.com/lists.json |
+    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' |
+    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' |
+    aria2c \
+      --input-file=- \
+      --dir="${CACHE}" \
+      --optimize-concurrent-downloads=true \
+      --stderr \
+      --max-connection-per-server="$(nproc)"
+
+  # and then rely on gunzip to output sequentially
+  gunzip --stdout "${CACHE}"/* |
+    grep -E -v '^#' |
+    gzip >blocklist.gz
+  # and cleanup afterward
+  rm -rf "${CACHE}"
+
+else
+
+  curl -s https://www.iblocklist.com/lists.json |
+    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' |
+    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' |
+    xargs wget -O - |
+    gunzip |
+    grep -E -v '^#' |
+    gzip >blocklist.gz
+fi
