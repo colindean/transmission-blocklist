dfa21a4868a3f1a86c5f7f56b1811261157e0020
Colin Dean <colindean@users.noreply.github.com>
2021-12-21 21:44:57 -0500

    Extract url retrival to a function

--- blocklist-build.sh
+++ blocklist-build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 : "${NO_CACHE:-}"
+
+get_urls() {
+  curl -s https://www.iblocklist.com/lists.json \
+  | jq --raw-output --from-file filter.jq \
+  | awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }'
+}
+
 # Use aria2c if available for ~300% speedup, mostly from parallel downloading
 if [ -z "${NO_CACHE}" ] && [ -n "$(command -v aria2c)" ]; then
   # however, aria2c can't output to stdout, so we have to write to a cache directory
   CACHE=$(mktemp -d cache-XXXXXX)
   mkdir -p "${CACHE}"
 
-  curl -s https://www.iblocklist.com/lists.json |
-    jq --raw-output --from-file filter.jq |
-    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' |
+  get_urls |
     aria2c \
       --input-file=- \
       --dir="${CACHE}" \
@@ -25,9 +30,7 @@ if [ -z "${NO_CACHE}" ] && [ -n "$(command -v aria2c)" ]; then
 
 else
 
-  curl -s https://www.iblocklist.com/lists.json |
-    jq --raw-output --from-file filter.jq |
-    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' |
+  get_urls |
     xargs wget -O - |
     gunzip |
     grep -E -v '^#' |
